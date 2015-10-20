bb9eb16c2fbcf1fdb2e1d47b04a28de0311a9cde
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2015-10-20 13:03:36 +0100

    Refactor: Automatically fetch content item
    So that controllers inheriting from ContentItemController don't need
    to `super` to get the content item set.
    
    Also handles 404s, rather than geenerating an error in the template by
    referencing an object that doesn't exist.

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -1,22 +1,31 @@
 require 'gds_api/content_store'
 
 class ContentItemsController < ApplicationController
+  before_filter :load_content_item, :only => :show
+
   def index
     @content_store_url = content_store_url
   end
 
   def show
-    @content_item = content_store.content_item(content_item_path)
   end
 
 protected
 
-  def content_store
-    @content_store ||= GdsApi::ContentStore.new(content_store_url)
+  def load_content_item
+    if content_item = content_store.content_item(content_item_path)
+      @content_item = content_item
+    else
+      render text: 'Not found', status: :not_found
+    end
   end
 
   def content_item_path
     '/' + URI.encode(params[:path])
+  end
+
+  def content_store
+    @content_store ||= GdsApi::ContentStore.new(content_store_url)
   end
 
   def content_store_url
