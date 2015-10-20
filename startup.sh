a7caa6e5b600e06155a1b82e1d380d305704493e
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2015-10-20 13:07:13 +0100

    Allow simpler service ENV setting for startup script
    The `PLEK_` params are long, hard to remember, easy to typo.
    
    Keep 'use-remote' versions of `static` / `content-store` as the default,
    but allow them to be set to local versions using simpler/shorter ENV vars.
    
    This also encapsulates the local paths for `static` / `content_store` to
    avoid having to know the ports / type full addresses.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,15 @@
 # Default to live static on GOV.UK, but still allow custom value if set
-if [ -z "$PLEK_SERVICE_STATIC_URI" ]; then
-    export PLEK_SERVICE_STATIC_URI="assets.digital.cabinet-office.gov.uk"
+
+if [ "$LOCAL_STATIC" ]; then
+    export PLEK_SERVICE_STATIC_URI="http://0.0.0.0:3013"
+elif [ -z "$PLEK_SERVICE_STATIC_URI" ]; then
+    export PLEK_SERVICE_STATIC_URI="http://assets.digital.cabinet-office.gov.uk"
+fi
+
+if [ "$LOCAL_CONTENT_STORE" ]; then
+    export PLEK_SERVICE_CONTENT_STORE_URI="http://0.0.0.0:3068"
+elif [ -z "$PLEK_SERVICE_CONTENT_STORE_URI" ]; then
+    export PLEK_SERVICE_CONTENT_STORE_URI="https://govuk-content-store-examples.herokuapp.com"
 fi
 
 bundle exec rails s
